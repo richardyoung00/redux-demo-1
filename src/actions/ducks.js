c9bc41d257edfb388de9b0b1b075a6d0b11e6ff1
Richard Young <richard@bnry.local>
2017-05-16 16:59:39 +0200

    add comments

--- src/actions/ducks.js
+++ src/actions/ducks.js
@@ -1,9 +1,11 @@
+// add new duck
 export const addDuck = () => {
   return {
     type: 'ADD_DUCK',
   }
 };
 
+// remove duck
 export const removeDuck = () => {
   return {
     type: 'REMOVE_DUCK',
